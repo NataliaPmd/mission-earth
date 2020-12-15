7b9ce37f2c6b9494023eaa561c897d3a6d765474
Natalia Pascual <natalia.pmd@gmail.com>
2020-12-15 20:17:27 +0100

    deploy configurations

--- server/index.js
+++ server/index.js
@@ -29,7 +29,9 @@ app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/upload', require('./routes/uploads') );
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public/index.html"));
+  });
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT );
